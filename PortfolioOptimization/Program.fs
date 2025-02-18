68a25cc36205740a425052c09f506e803ee49760
MaksymStefurak <116295809+MaksymStefurak@users.noreply.github.com>
2025-02-18 17:13:32 +0200

    Update Program.fs
    Commit changes

--- PortfolioOptimization/Program.fs
+++ PortfolioOptimization/Program.fs
@@ -2,11 +2,9 @@
 open MathNet.Numerics.LinearAlgebra
 open MathNet.Numerics.Statistics
 
-// Явно задаем фабрику для работы с матрицами и векторами
 let vector = Vector<float>.Build
 let matrix = Matrix<float>.Build
 
-// Пример исторических доходностей активов (возвраты активов)
 let historicalReturns = 
     array2D [ 
         [ 0.01;  0.02; -0.01 ]
@@ -16,28 +14,22 @@ let historicalReturns =
         [ 0.01;  0.03;  0.02 ]
     ]
 
-// Создаем матрицу на основе корректного типа данных
 let historicalMatrix = matrix.DenseOfArray(historicalReturns)
 
-// Вычисление средних доходностей активов
 let meanReturns = historicalMatrix.ColumnSums() / float historicalMatrix.RowCount
 
-// Вычисление ковариационной матрицы
 let covarianceMatrix =
     let cols = historicalMatrix.ColumnCount
     matrix.Dense(cols, cols, fun i j -> 
         Statistics.Covariance(historicalMatrix.Column(i).ToArray(), historicalMatrix.Column(j).ToArray()))
 
-// Функция оценки риска портфеля (дисперсия)
 let portfolioVariance (weights: Vector<float>) =
     weights * covarianceMatrix * weights
 
-// Генерация случайных весов портфеля
 let randomWeights numAssets =
     let rawWeights = vector.Random(numAssets)
     rawWeights / rawWeights.Sum()
 
-// Поиск оптимального портфеля методом случайного поиска
 let findOptimalPortfolio numIterations numAssets =
     let mutable bestWeights = randomWeights numAssets
     let mutable minRisk = portfolioVariance bestWeights
@@ -49,9 +41,7 @@ let findOptimalPortfolio numIterations numAssets =
             bestWeights <- newWeights
     bestWeights, minRisk
 
-// Запуск оптимизации на 1000 итераций для 3 активов
 let optimalWeights, minRisk = findOptimalPortfolio 1000 historicalMatrix.ColumnCount
 
-// Вывод результата
 printfn "Оптимальные веса портфеля: %A" optimalWeights
 printfn "Минимальный риск: %f" minRisk
